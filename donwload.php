0ce02c7826579ae9edd67357a5982381b80df75e
KostsPlayer <114208201+KostsPlayer@users.noreply.github.com>
2023-01-16 10:16:49 +0700

    Delete donwload.php

--- donwload.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if(isset($_GET['file']))
-    {
-        header("Content-DIsposition: attachment; filename = ") . basename($_GET['file']);
-        readfile($_GET['file']);
-        exit();
-    }
-?>
